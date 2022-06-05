da677411f7842e10940d42a796722d8a2f92df87
govella <govella08@gmail.com>
2022-06-05 23:39:50 +0300

    Added MainNavigation and links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Route, Switch, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AllMeetupPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetupPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
     </div>
   );
---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const MainNavigation = () => {
+  return (
+    <header>
+      <div>React Meetup</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetup</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">New Meetup</Link>
+          </li>
+          <li>
+            <Link to="/favorites">My Favorite</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
